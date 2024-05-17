6ad186214f16fe2c111535d62c7e2ed253ed4c59
vdavidmarques <vdavidmarques@gmail.com>
2024-05-17 17:52:04 -0300

    chore: Defing header style

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -1,28 +1,43 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+<html <?php language_attributes(); ?>>
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width">
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width">
+    <?php wp_head(); ?>
+    <title itemprop="name">
+        Portólio Vinícius Marques <?php if (wp_title()) :  ' | ' . wp_title();
+                                    endif; ?>
+    </title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 </head>
+
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<div id="branding">
-<div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-<?php
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
-echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
-?>
-</div>
-<div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
-</div>
-<nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
-<div id="search"><?php get_search_form(); ?></div>
-</nav>
-</header>
-<div id="container">
-<main id="content" role="main">
+    <header>
+        <div class="header-menu-itens container">
+            <a href="/portfolio" class="logo">
+                Portólio
+            </a>
+            <div class="open-menu-mobile">
+                <button onclick="openMenu()" class="bg-menu-mobile">
+                    Menu
+                </button>
+            </div>
+            <div class="menu-items">
+                <?php custom_header_menu(); ?>
+
+                <button onclick="closeMenu()" class="bg-close-menu-mobile">
+                    Fechar menu
+                </button>
+            </div>
+        </div>
+    </header>
+
+    <main id="content" role="main">
